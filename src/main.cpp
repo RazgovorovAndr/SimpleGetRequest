ca0d5c7abed950e65a4b03722b5295bfc0dde095
razgovorov andrei <razgovorovandr@gmail.com>
2020-09-11 00:37:49 +0300

    http_header class declartion

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <http_headers.hpp>
 
 int main(int argc, char **argv)
 {
-    int t = 1;
-    std::cout << "Hello, world!";
+    std::cout << "Hello, world!"<< std::endl;;
+    HttpHeaders headers{};
+    headers.SetHeader(L"sid", L"4214-532-fde");
+    std::wcout << headers.GetHeader(L"sid") << std::endl;
+    std::wcout << headers.GetHeader(L"keep-alive") << std::endl;
     return 0;
 }
