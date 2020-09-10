258c3b5ac44aff8249ae40c728eba5b4f230281c
razgovorov andrei <razgovorovandr@gmail.com>
2020-09-11 00:42:14 +0300

    remove extra include

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <http_headers.hpp>
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello, world!"<< std::endl;;
-    HttpHeaders headers{};
-    headers.SetHeader(L"sid", L"4214-532-fde");
-    std::wcout << headers.GetHeader(L"sid") << std::endl;
-    std::wcout << headers.GetHeader(L"keep-alive") << std::endl;
+    std::cout << "Hello, world!"<< std::endl;
     return 0;
 }
